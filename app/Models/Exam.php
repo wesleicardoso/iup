710e07c1022acc804e91ff8c2132587783773e44
wesleicardoso <150357484+wesleicardoso@users.noreply.github.com>
2025-11-30 01:18:38 -0300

    feat: att

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -8,7 +8,7 @@ class Exam extends Model
 {
     protected $guarded = [];
 
-    // Relação: Um exame pode ser exigido por várias funções
+    // Relação: Um exame pode ser exigido por funções
     public function roles()
     {
         return $this->belongsToMany(Role::class);
